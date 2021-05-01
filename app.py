281d51d7967b62ba36a0bb5e25cbb8b8c96d7fc3
yunine16 <yuchin616@icloud.com>
2021-05-02 01:40:11 +0900

    ページを追加 upload, main, result画面

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html", message="Hello world!")
+    return render_template("index.html")
+
+@app.route('/upload')
+def upload():
+    return render_template("upload.html")
+
+@app.route('/main')
+def main():
+    return render_template("main.html")
+
+@app.route('/result')
+def result():
+    return render_template("result.html")
 
 if __name__ == '__main__':
     app.debug = True
